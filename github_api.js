11ce821ce59710697d5b592d809637f61032e786
= <alexm91@vt.edu>
2023-05-02 08:51:57 -0700

    cors test

--- github_api.js
+++ github_api.js
@@ -2,7 +2,7 @@ console.log('In github_api.js');
 
 async function github_auth() {
     let response = await fetch('https://github.com/login/oauth/authorize', {
-        // mode: 'no-cors',    
+        mode: 'no-cors',    
         client_id: '02bae144ed96d86c63f6',
         redirect_uri: 'https://alex-monahan.github.io/duckdb_wasm_github_pages.github.io/',
         state: 'ZYXABC',
