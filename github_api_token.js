06510571bb946f279c0a8fb4e0f56020d53ddfec
= <alexm91@vt.edu>
2023-05-03 07:30:12 -0700

    prompt for token

---
+++ github_api_token.js
@@ -0,0 +1,13 @@
+import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
+
+async function test_auth() {
+    // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+    let token = prompt("Please enter your github personal access token");
+    const octokit = new Octokit({ auth: token });
+
+    const response = await octokit.request("GET /orgs/{org}/repos", {
+        org: "octokit",
+        type: "private",
+    });
+    console.log('response.json()',response.json());
+}
